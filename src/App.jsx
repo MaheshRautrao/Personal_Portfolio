167152c697c508e2bd4d462544fd856fdfb5431c
MaheshRautrao <101188065+MaheshRautrao@users.noreply.github.com>
2023-12-29 21:51:17 +0530

    started working on sidenav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import MainContainer from "./components/MainContainer/MainContainer";
+import SideNavbar from "./components/sideNavBar/SideNavbar";
+
 function App() {
-  return <>mahesh</>;
+  return (
+    <div className="flex flex-col">
+      <SideNavbar />
+      <MainContainer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/sideNavBar/SideNavbar.jsx
@@ -0,0 +1,5 @@
+const SideNavbar = () => {
+  return <div className="bg-blue-400">SideNavbar</div>;
+};
+
+export default SideNavbar;
